b69d20bcbad3fc1e5637fa246c3c6f6dc46a8c29
DESKTOP-3F4BINV\Szymon <Szymek_diks@interia.pl>
2017-09-21 23:37:59 +0200

    Little clear out

--- NonLinearProgramming/src/main/java/DifferentDimensionException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exception created to inform about dimensions inequality double tables.
- *
- * Created by Szymon on 15.05.2017.
- */
-public class DifferentDimensionException extends Exception {
-
-}
